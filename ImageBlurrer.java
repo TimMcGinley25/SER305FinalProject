957ddf673571579dca0dc4e7da620a5904f796ce
Tim McGinley <timmcginley0325@gmail.com>
2024-05-04 18:56:46 -0400

    Updated blur and applied blur methods

--- ImageBlurrer.java
+++ ImageBlurrer.java
@@ -2,56 +2,73 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.io.File;
 import java.io.IOException;
 
 public class ImageBlurrer extends JFrame {
-	private JSlider blurSlider;
-	private JLabel imageLabel;
-	private BufferedImage originalImage;
-	private BufferedImage newImage;
+    private JSlider blurSlider;
+    private JLabel imageLabel;
+    private BufferedImage originalImage;
+    private BufferedImage newImage;
 
-	public ImageBlurrer() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setTitle("Image Blurrer");
-		setSize(800, 600);
-		setLocationRelativeTo(null);
-		setLayout(new BorderLayout());
+    public ImageBlurrer() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Image Blurrer");
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
 
-		try {
-			originalImage = ImageIO.read(new File("image.png"));
-			imageLabel = new JLabel(new ImageIcon(originalImage));
-		} catch (IOException e) {
-			e.printStackTrace();
-			imageLabel = new JLabel("Unable to load image.png");
-		}
-		add(imageLabel, BorderLayout.CENTER);
+        try {
+            originalImage = ImageIO.read(new File("pik.png"));
+            imageLabel = new JLabel(new ImageIcon(originalImage));
+        } catch (IOException e) {
+            e.printStackTrace();
+            imageLabel = new JLabel("Unable to load pik.png");
+        }
+        add(imageLabel, BorderLayout.CENTER);
 
-		blurSlider = new JSlider(0, 20, 0);
+        blurSlider = new JSlider(0, 20, 0);
         blurSlider.setMajorTickSpacing(5);
         blurSlider.setMinorTickSpacing(1);
         blurSlider.setPaintTicks(true);
         blurSlider.setPaintLabels(true);
         blurSlider.addChangeListener(e -> updateBlur());
         add(blurSlider, BorderLayout.SOUTH);
+    }
 
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            ImageBlurrer frame = new ImageBlurrer();
+            frame.setVisible(true);
+        });
+    }
 
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {
-			ImageBlurrer frame = new ImageBlurrer();
-			frame.setVisible(true);
-		});
-	}
+    private void updateBlur() {
+        int blurIntensity = blurSlider.getValue();
+        newImage = blur(originalImage, blurIntensity);
+        imageLabel.setIcon(new ImageIcon(newImage));
+    }
 
-	private void updateBlur() {
-		newImage = applyBlur(originalImage, blurSlider.getValue());
-		imageLabel.setIcon(new ImageIcon(newImage));
-	}
+    public static BufferedImage blur(BufferedImage image, int blurIntensity) {
+        if (blurIntensity < 1) {
+            return image;
+        }
 
-	private BufferedImage applyBlur(BufferedImage src, float radius) {
-		return src.getSubimage(0, 0, (int) ((1 + radius) * 8), (int) ((1 + radius) * 8));
-	}
+        if (blurIntensity % 2 == 0) {
+            blurIntensity++;
+        }
 
+        float[] matrix = new float[blurIntensity * blurIntensity];
+        float blurFactor = 1.0f / (blurIntensity * blurIntensity);
+        for (int i = 0; i < matrix.length; i++) {
+            matrix[i] = blurFactor;
+        }
 
+        BufferedImageOp op = new ConvolveOp(new Kernel(blurIntensity, blurIntensity, matrix),
+                ConvolveOp.EDGE_NO_OP, null);
+        return op.filter(image, null);
+    }
 }
